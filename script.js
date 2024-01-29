bdbcb4f40eb7451f809c041944d421b5adacafdf
moonymel <musumeci.me@gmail.com>
2024-01-29 15:22:00 +0100

    [UPDATE] set files, getting strings from todo-list.json in server.php

---
+++ script.js
@@ -0,0 +1,15 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+
+        }
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    }
+}).mount('#app')
---
+++ server.php
@@ -0,0 +1,7 @@
+<?php 
+
+    //recupero file json
+    $todo_list = file_get_contents('todo-list.json');
+    var_dump($todo_list);
+
+?>
